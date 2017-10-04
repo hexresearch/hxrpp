67e6022d6313d840c4ae1cd22594868e49208c0f
Dmitry Zuikov <dzuikov@gmail.com>
2017-10-04 10:21:10 +0300

    wip

---
+++ src/hexsockaddr_platform.h
@@ -0,0 +1,12 @@
+#ifndef __hexsockaddr_platform_h
+#define __hexsockaddr_platform_h
+
+#include <arpa/inet.h>
+#include <stdint.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <stdio.h>
+
+
+#endif
--- src/hxrpp_server.c
+++ src/hxrpp_server.c
@@ -28,10 +28,56 @@
 #include <inttypes.h>
 
 #include "utils/app_config.h"
+#include "hxrpp_server.h"
+
+struct hxrpp_server_cfg {
+    bool fake;
+    struct app_config *cfg;
+    char cfg_mem[0];
+};
+
+
+#define APP_CONFIG_SIZE ((sizeof(struct hxrpp_server_cfg)) + app_config_size)
+
+static void * __alloc(void *cc, size_t n) {
+    return malloc(n);
+}
+
+static void __dealloc(void *cc, void *mem) {
+    free(mem);
+}
+
+
+static struct hxrpp_server_cfg *init_app_config(void *mem, size_t size) {
+    assert( size >= APP_CONFIG_SIZE );
+
+    memset(mem, 0, size);
+
+    struct hxrpp_server_cfg *app = mem;
+    app->cfg = (void*)app->cfg_mem;
+
+    struct app_config *cfg =
+        app_config_create( app_config_size
+                         , app->cfg_mem
+                         , 0
+                         , __alloc
+                         , __dealloc
+                         );
+
+    return app;
+}
 
 int main(int argc, char **argv)
 {
-    fprintf(stdout, "JOPA\n\n");
+    // TODO: init with command line args
+    char conf_mem[APP_CONFIG_SIZE];
+
+    struct hxrpp_server_cfg *app = init_app_config(conf_mem, sizeof(conf_mem));
+
+    if( !app_config_load(app->cfg, argc, argv) ) {
+        exit(-1);
+    }
+
 
     // TODO: bind to UDP socket
     // TODO: listen UDP socket
@@ -42,3 +88,7 @@ int main(int argc, char **argv)
 
     return 0;
 }
+
+
+
+
